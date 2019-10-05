e99756e839c556f0965f8bb0d4347a0532f8e44f
tyelf22 <tyelf22@hotmail.com>
2019-10-05 00:59:59 -0600

    added comments

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-console.log('hello world')
-
-
 
 resize = () => {
     let width = window.innerWidth;
@@ -13,6 +10,8 @@ resize = () => {
 
     let title = document.title;
     let modify = document.lastModified;
+
+    //set inner text to display correct elements
     document.querySelector('#winProp').innerText = `The window size is ${width} pixels wide and ${height} pixels tall \n Window offset is ${x} from the left and ${y} from the top of the display \n The page URL is ${url}`;
     document.querySelector('#docProp').innerText = `Document title is ${title} \n This document was last updated on ${modify}`;
 };
